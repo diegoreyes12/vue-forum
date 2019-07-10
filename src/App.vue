c242cf531cefcbb714bed63af3ca5dbb7dd5f1c5
Juan Reyes <reyes1212@gmail.com>
2019-07-09 23:21:42 -0700

    wip revert nav router link and style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
     <div class="container">
       <router-view />
     </div>
@@ -8,4 +12,14 @@
 
 <style>
 @import "assets/css/style.css";
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
